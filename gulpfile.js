737f0589ee2fad5ea15c93821557f24e4e876d38
Harrison Lingren <harrisonlingren@gmail.com>
2016-08-31 15:36:44 -0400

    include src/js/app/components in gulp, reorder compiliation

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,7 @@ var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
 
 gulp.task("default", function () {
-  return gulp.src(['src/js/vendor/jquery/dist/jquery.js', 'src/js/app/*.js'])
+  return gulp.src(['src/js/vendor/jquery/dist/jquery.js', 'src/js/app/components/*.js', 'src/js/app/app.js'])
     .pipe(sourcemaps.init())
     .pipe(concat("all.js"))
     .pipe(rename('app.min.js'))
