eb6b5512279ba49bc920f6bd0974540cc28d36d1
Harrison Lingren <harrisonlingren@gmail.com>
2016-08-31 11:56:34 -0400

    fix build scripts to use gulp, bower and httpster locally

--- build.sh
+++ build.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 echo "[Info] Installing dependencies..."
-bower install
+./node_modules/bower/bin/bower install
 
 echo "[Info] Building source..."
-gulp
+./node_modules/gulp/bin/gulp.js
 
 cp ./src/index.html ./build/index.html
 cp ./src/js/vendor/aframe/dist/aframe-v0.3.0.min.js ./build/aframe.min.js
 cp -r ./src/assets ./build/
 echo "[Info] Build successful! Compiled js/css/html located in ./build "
 
-# echo "[Info] Starting HTTPster in ./build ..."
-# httpster -d build
+echo "[Info] Starting HTTPster in ./build ..."
+./node_modules/httpster/bin/httpster -d build
