25eec31102d37c86c62559881d05aa8c31386ab6
Harrison Lingren <harrisonlingren@gmail.com>
2016-08-31 18:00:48 -0400

    fix laser behaviior

--- src/js/app/components/spawner.js
+++ src/js/app/components/spawner.js
@@ -18,9 +18,18 @@ AFRAME.registerComponent('spawner', {
     var matrixWorld = el.object3D.matrixWorld;
     var position = new THREE.Vector3();
     position.setFromMatrixPosition(matrixWorld);
+
+    var rotation = "0 0 0";
+    var rotationX = el.object3D.rotation.x;
+    var rotationY = el.object3D.rotation.y;
+    var rotationX = el.object3D.rotation.z;
+
+    rotation = rotationX.toString + rotationY.toString + rotationZ.toString;
+
     var entity = document.createElement('a-entity');
     entity.setAttribute('position', position);
     entity.setAttribute('mixin', this.data.mixin);
+    entity.setAttribute('rotation', rotation);
     el.sceneEl.appendChild(entity);
   }
 });
